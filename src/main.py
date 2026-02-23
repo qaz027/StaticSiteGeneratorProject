efbbc1955071cbbd846e77bdfdcc20100c85b70c
QuimbanoPrime <qaz027@pm.me>
2026-02-22 20:01:16 -0800

    finished the textnode setup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hello World.")
+from textnode import *
+
+#print("Hello World.")
+
+def main():
+    dummy = TextNode('this is some dummy text', TextType.LINKS, 'https://boot.dev/sexual_chocolate')
+    print(dummy.__repr__())
+
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,20 +1,23 @@
 from enum import Enum
 
 class TextType(Enum):
-    PLAIN = "plain" #text (plain)
+    PLAIN = "text" #text (plain)
     BOLD = "bold" #**Bold text**
-    ITALICS = "italics" #_Italic text_
+    ITALICS = "italic" #_Italic text_
     CODE = "code" #`Code text`
-    LINKS = "links" #Links, in this format: [anchor text](url)
+    LINKS = "link" #Links, in this format: [anchor text](url)
     IMAGES = "images" #Images, in this format: ![alt text](url)
 
-class TextNode(text, text_type, url=None):
-    self.text = text
-    self.text_type = text_type
-    self.url = url
+class TextNode:
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
 
     def __eq__(self, other):
-        if self.text == other.text & self.text_type == other.text_type & self.url == other.url
+        if self.text == other.text & self.text_type == other.text_type & self.url == other.url:
+            return True
+        return False
 
     def __repr__(self):
-        return f"TextNode({self.text.upper()}, {self.text_type.upper()}, {self.url.upper()})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
