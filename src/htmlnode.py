8b4498231752fc535463f883d5615aa61b756baf
QuimbanoPrime <qaz027@pm.me>
2026-02-24 21:02:58 -0800

    adding HTML node and test

---
+++ src/htmlnode.py
@@ -0,0 +1,23 @@
+
+
+class HTMLNode():
+
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag  # A string representing the HTML tag name (e.g. "p", "a", "h1", etc.)
+        self.value = value # A string representing the value of the HTML tag (e.g. the text inside a paragraph)
+        self.children = children # A list of HTMLNode objects representing the children of this node
+        self.props = props # A dictionary of key-value pairs representing the attributes of the HTML tag. For example, a link (<a> tag) might have {"href": "https://www.google.com"}
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self): 
+        if self.props is None:
+            return ""
+        return_str = ''
+        for prop in self.props:
+            return_str += f' {prop}="{self.props[prop]}"'
+        return return_str
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
